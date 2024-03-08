ea482c4b113d498a9b427d0c3c7aa8be03b24244
hawaesanda <hawa.esanda@gmail.com>
2024-03-08 20:36:57 +0700

    Add: Jobsheet4 Practicum 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,11 +10,12 @@ class UserController extends Controller
 {
     public function index()
     {
+        //Jobsheet 3
         //tambah data user dengan Eloquent Model
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); //update data user
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data); //update data user
         // $data = [
         //     'username' => 'customer-1',
         //     'nama' => 'Pelanggan',
@@ -22,6 +23,15 @@ class UserController extends Controller
         //     'level_id' => 4
         // ];
         // UserModel::insert($data); //tambahkan data ke tabel m_user
+        
+        //Jobsheet4 practicum1
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
         
         //coba akses model usermodel
         $user = UserModel::all(); //ambil semua data dari tabel m_user
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,5 +11,13 @@ class UserModel extends Model
 
     protected $table = 'm_user'; //mendefinisikan nama table yang digunakan oleh model
     protected $primaryKey = 'user_id'; //mendefinisikan primary key dari tabel yang digunakan
-    
+    //Jobsheet4 P1
+    /**
+     * The attributes that are mass assignable
+     * 
+     * @var array
+     */
+    protected $fillable = ['level_id', 'username', 'nama'];
 }
+//3. When running localhost/PWL_POS/public/user the web page will display the user data entered, namely 'manager_two' as well as the database will increase by 1 row.
+//6. An error 'SQLSTATE[HY000]: General error: 1364 The 'password' field has no default value' This is because in the UserModel, the password field is not populated with a default value and is not considered a mass assignable field.
