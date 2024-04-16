76124b69c1b6d50cd1e4a8f6bf6eb179aa0b948e
hawaesanda <hawa.esanda@gmail.com>
2024-04-16 19:51:58 +0700

    Jobsheet 7: Practicum 3 Implementation of jQuery Datatable in AdminLTE

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,157 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LevelModel;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\DataTables\UserDataTable;
 use Illuminate\Http\RedirectResponse;
+use Yajra\DataTables\Facades\DataTables;
+
 
 class UserController extends Controller
 {
+    //Menampilkan halaman awal user
+    public function index()
+    {
+        $breadcrumb = (object) [
+            'title' => 'Daftar User',
+            'list' => ['Home', 'User']
+        ];
+
+        $page = (object) [
+            'title' => 'Daftar user yang terdaftar dalam sistem'
+        ];
+
+        $actiiveMenu = 'user'; //setmenu yang sedang aktif
+
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $actiiveMenu]);
+    }
+
+    public function list(Request $request)
+    {
+        $users = UserModel::select('user_id', 'username', 'nama', 'level_id') -> with('level');
+
+        return DataTables::of($users)
+        ->addIndexColumn() // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
+        ->addColumn('action', function ($user) { //add action column
+            $btn = '<a href="'.url('/user/' . $user->user_id).'" class="btn btn-info btn-sm">Detail</a> ';
+            $btn .= '<a href="' . url('/user/' . $user->user_id . '/edit' ) . '" class="btn btn-warning btn-sm">Edit</a> ';
+            $btn .= '<form class="d-inline-block" method="POST" action="'. url('/user/'.$user->user_id).'">'
+            .csrf_field() . method_field('DELETE') .
+            '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Are you sure to delete this data?\');">Delete</button></form>';
+            return $btn;
+        })
+        ->rawColumns(['action']) // tells that the action is html
+        ->make(true);
+    }
+    
+    public function create()
+    {
+        $breadcrumb = (object) [
+            'title' => 'Tambah User',
+            'list' => ['Home', 'User', 'Tambah']
+        ];
+
+        $page =(object) [
+            'title' => 'Tambah user baru'
+        ];
+
+        $level = LevelModel::all(); //ambil data level untuk ditampilkan di form
+        $activeMenu ='user'; //set menu yang sedang aktif
+
+        return view('user.create', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            //username harus diisi, berupa string, minimal 3 karakter, dan bernilai unik di tabel m_user kolom username
+            'username' => 'required|string|min:3|unique:m_user,username',
+            'nama' => 'required|string|max:100',
+            'password' => 'required|min:5',
+            'level_id' => 'required|integer'
+        ]);
+
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => bcrypt($request->password), //password dienkripsi sebelumdisimpan
+            'level_id' => $request->level_id
+        ]);
+
+        return redirect('/user')->with('success', 'Data user berhasil disimpan');
+    }
+
+    public function show(string $id)
+    {
+        $user = UserModel::with('level')->find($id);
+
+        $breadcrumb = (object) [
+            'title' => 'Detail User',
+            'list' => ['Home', 'User', 'Detail']
+        ];
+
+        $page = (object) [
+            'title' => 'Detail User'
+        ];
+
+        $activeMenu = 'user';
+
+        return view('user.show', ['breadcrumb' => $breadcrumb, 'page' => $page, 'user' => $user, 'activeMenu' => $activeMenu]);
+    }
+
+    public function edit(string $id)
+    {
+    $user = UserModel::find($id);
+    $level = LevelModel::all();
+    $breadcrumb = (object)[
+        'title' => 'Edit User',
+        'list' => ['Home', 'User', 'Edit']
+    ];
+
+    $activeMenu = 'user'; //set menu yang sedang aktif
+    $page = (object)['title' => 'Edit User']; // Inisialisasi $page dengan judul yang sesuai
+
+    return view('user.edit', ['breadcrumb' => $breadcrumb, 'page' => $page, 'user' => $user, 'level' => $level, 'activeMenu' => $activeMenu]);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'username'  => 'required|string|min:3|unique:m_user,username,'.$id.',user_id',
+            'nama'      => 'required|string|max:500',
+            'password'  => 'nullable|min:5',
+            'level_id'  => 'required|integer'
+        ]);
+
+        UserModel::find($id)->update([
+            'username'  => $request->username,
+            'nama'      => $request->nama,
+            'password'  => $request->password ? bcrypt($request->password) : UserModel::find($id)->password,
+            'level_id'  => $request->level_id
+        ]);
+
+        return redirect('/user')->with('success', 'Data user berhasil diubah');
+    }
+
+    public function destroy(string $id)
+    {
+        $check = UserModel::find($id);
+        if (!$check){
+            return redirect('/user')->with('eror', 'Data user tidak ditemukan');
+        }
+
+        try{
+            UserModel::destroy($id);
+            return redirect('/user')->with('success', 'Data user berhasil dihapus');
+        }catch(\Illuminate\Database\QueryException $e){
+            //Jika terjadi eror ketika menghapus data, redirect kembali ke halaman dnegan membawa pesan eror
+            return redirect('/user')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
+        }
+    }
+
     // public function index()
     // {
         //Jobsheet 3
@@ -172,48 +315,48 @@ class UserController extends Controller
 //14. The web page will be show 'Form Ubah Data User' when cliking 'Change' button.
 //17. When clicking the 'Change' button, the data will change.
 //19. When clicking the 'Hapus' button, the selected data will be deleted
-    public function index(UserDataTable $dataTable){
-        return $dataTable->render('user.index');
-    }
-    public function create(){
-        return view('user.create');
-    }
-    public function tambah_simpan(Request $request): RedirectResponse{
-        // UserModel::create
-        $validated= $request->validate([
-            // 'username' => $request->username,
-            // 'nama' => $request->nama,
-            // 'level_id' => $request->level_id,
-            // 'password' => Hash::make($request->password)
-            'username'=>'required',
-            'nama'=>'required',
-            'level_id'=>'required',
-            'password'=>'required'
-        ]);
+    // public function index(UserDataTable $dataTable){
+    //     return $dataTable->render('user.index');
+    // }
+    // public function create(){
+    //     return view('user.create');
+    // }
+    // public function tambah_simpan(Request $request): RedirectResponse{
+    //     // UserModel::create
+    //     $validated= $request->validate([
+    //         // 'username' => $request->username,
+    //         // 'nama' => $request->nama,
+    //         // 'level_id' => $request->level_id,
+    //         // 'password' => Hash::make($request->password)
+    //         'username'=>'required',
+    //         'nama'=>'required',
+    //         'level_id'=>'required',
+    //         'password'=>'required'
+    //     ]);
 
-        return redirect('/user');
-    }
-    public function ubah($id)
-    {
-        $user = UserModel::find($id);
-        return view('user_ubah', ['data'=>$user]);
-    }
-    public function ubah_simpan($id, Request $request){
-        $user = UserModel::find($id);
+    //     return redirect('/user');
+    // }
+    // public function ubah($id)
+    // {
+    //     $user = UserModel::find($id);
+    //     return view('user_ubah', ['data'=>$user]);
+    // }
+    // public function ubah_simpan($id, Request $request){
+    //     $user = UserModel::find($id);
 
-        $user->username = $request->username;
-        $user->nama = $request->nama;
-        $user->level_id = $request->level_id;
+    //     $user->username = $request->username;
+    //     $user->nama = $request->nama;
+    //     $user->level_id = $request->level_id;
 
-        $user->save();
-        return redirect('/user');
-    }
-    public function hapus($id)
-    {
-        $user = UserModel::find($id);
-        $user->delete();
+    //     $user->save();
+    //     return redirect('/user');
+    // }
+    // public function hapus($id)
+    // {
+    //     $user = UserModel::find($id);
+    //     $user->delete();
 
-        return redirect('/user');
-    }
+    //     return redirect('/user');
+    // }
 
 }
--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,44 +1,67 @@
-@extends('layouts.app') 
-
-@section('subtitle', 'Kategori')
-@section('content_header_title', 'User')
-@section('content_header_subtitle', 'Tambah')
-
-{{-- @section('content_header') 
-    <h1>Form Tambah User</h1> 
-@stop  --}}
+@extends('layouts.template') 
 
 @section('content') 
-<div class="card card-primary">
-    <div class="card-header">
-      <h3 class="card-title">Form Tambah User</h3>
-    </div>
-    <!-- /.card-header -->
-    <!-- form start -->
-    <form method="POST" action="../user">
+<div class="card card-outline card-primary">
+  <div class="card-header">
+    <h3 class="card-title">{{ $page->title }}</h3>
+    <div class="card-tools"></div>
+  </div>
+  <div class="card-body">
+    <form method="POST" action="{{ url('user') }}" class="form-horizontal">
       @csrf
-      <div class="card-body">
-        <div class="form-group">
-          <label for="username">Username</label>
-          <input type="text" class="form-control" id="username" name="username" placeholder="Masukkan Username">
-        </div>
-        <div class="form-group">
-          <label for="nama">Nama</label>
-          <input type="text" class="form-control" id="nama" name="nama" placeholder="Masukkan Nama">
-        </div>
-        </div>
-        <div class="form-group">
-          <label for="level_id">Level ID</label>
-          <input type="Number" class="form-control" id="level_id" name="level_id" placeholder="Masukkan Level ID">
-        </div>
-        <div class="form-group">
-          <label for="password">Password</label>
-          <input type="text" class="form-control" id="password" name="password" placeholder="Masukkan Password">
+      <div class="form-group row">
+        <label class="col-1 control-label col-form-label">Level</label>
+        <div class="col-11">
+          <select class="form-control"  id="level_id" name="level_id" required>
+            <option value="">- Pilih Level -</option>
+            @foreach ($level as $item)
+                <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+            @endforeach
+          </select>
+          @error('level_id')
+              <small class="form-text text-danger">{{ $message }}</small>
+          @enderror
         </div>
       </div>
-      <div class="card-footer">
-        <button type="submit" class="btn btn-primary">Submit</button>
+      <div class="form-group row">
+        <label class="col-1 control-label col-form-label">Username</label>
+        <div class="col-11"> 
+          <input type="text" class="form-control" id="username" name="username" value="{{ old('username') }}" required> 
+          @error('username') 
+            <small class="form-text text-danger">{{ $message }}</small> 
+          @enderror
+        </div>
+      </div>
+      <div class="form-group row"> 
+        <label class="col-1 control-label col-form-label">Nama</label> 
+        <div class="col-11"> 
+          <input type="text" class="form-control" id="nama" name="nama" value="{{ old('nama') }}" required> 
+          @error('nama') 
+            <small class="form-text text-danger">{{ $message }}</small> 
+          @enderror 
+        </div> 
+      </div> 
+      <div class="form-group row"> 
+        <label class="col-1 control-label col-form-label">Password</label> 
+        <div class="col-11"> 
+          <input type="password" class="form-control" id="password" name="password" required>             @error('password') 
+            <small class="form-text text-danger">{{ $message }}</small> 
+          @enderror 
+        </div> 
+      </div>
+      <div class="form-group row"> 
+        <label class="col-1 control-label col-form-label"></label> 
+        <div class="col-11"> 
+          <button type="submit" class="btn btn-primary btn-sm">Simpan</button> 
+          <a class="btn btn-sm btn-default ml-1" href="{{ url('user') }}">Kembali</a> 
+        </div> 
       </div>
     </form>
   </div>
-@endsection
+</div>
+
+@endsection 
+@push('css') 
+@endpush 
+@push('js') 
+@endpush 
