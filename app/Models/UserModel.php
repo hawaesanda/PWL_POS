e3f040ec6e3ce13bed68e876398999e5bf70ac5d
hawaesanda <hawa.esanda@gmail.com>
2024-05-12 13:34:35 +0700

    Jobsheet10 : Practicum 1 – Creating a RESTful API Register

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,26 +6,41 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as UserAuthenticate;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends UserAuthenticate
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
-
-    protected $table = 'm_user'; //mendefinisikan nama table yang digunakan oleh model
-    protected $primaryKey = 'user_id'; //mendefinisikan primary key dari tabel yang digunakan
-    
-    //Jobsheet4 P1
-    /**
-     * The attributes that are mass assignable
-     * 
-     * @var array
-     */
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
-
-    public function level(): BelongsTo
-    {
-        return $this->belongsTo(levelModel::class, 'level_id', "level_id");
+    public function getJWTIdentifier(){
+        return $this->getKey();
     }
+
+    public function getJWTCustomClaims(){
+        return [];
+    }
+
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+    protected $fillable = ['username','nama','level_id', 'password'];
 }
-//3. When running localhost/PWL_POS/public/user the web page will display the user data entered, namely 'manager_two' as well as the database will increase by 1 row.
-//6. An error 'SQLSTATE[HY000]: General error: 1364 The 'password' field has no default value' This is because in the UserModel, the password field is not populated with a default value and is not considered a mass assignable field.
+// class UserModel extends UserAuthenticate
+// {
+//     use HasFactory;
+
+//     protected $table = 'm_user'; //mendefinisikan nama table yang digunakan oleh model
+//     protected $primaryKey = 'user_id'; //mendefinisikan primary key dari tabel yang digunakan
+    
+//     //Jobsheet4 P1
+//     /**
+//      * The attributes that are mass assignable
+//      * 
+//      * @var array
+//      */
+//     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+//     public function level(): BelongsTo
+//     {
+//         return $this->belongsTo(levelModel::class, 'level_id', "level_id");
+//     }
+// }
+
